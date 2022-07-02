be4f317f9e12e76903112080c67a5673bfc6bd8e
IamMSP <105362180+IamMSP@users.noreply.github.com>
2022-07-02 18:07:02 +0100

    My First Zuri Django-API project

---
+++ venv/links/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.template.defaultfilters import slugify
+
+
+
+# Create your models here.
+# creating a model Link with some fields
+class Link(models.Model):
+    target_url = models.URLField(max_length = 200)
+    description = models.CharField(max_length = 200)
+    identifier = models.SlugField(max_length = 200, blank = True, unique = True)
+    author = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    created_date = models.DateTimeField (auto_now_add = True)
+    active = models.BooleanField(default = True)
+    
+    def save(self, *args, **kwargs):
+        self.identifier = slugify(self.target_url)
+        super(Link, self).save(*args, **kwargs)
+        
